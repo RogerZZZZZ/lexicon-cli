7f5c88b5a1e08e13435073c1eb8f520d204027c0
RogerZZZZZ <501841352@qq.com>
2018-11-09 00:08:57 +0800

    add class to store result

--- server/crawler.js
+++ server/crawler.js
@@ -4,6 +4,14 @@ const c = new Crawler({
   maxConnections: 1,
 })
 
+class Word {
+  constructor(symbol, translation, changes) {
+    this.symbol = symbol
+    this.translation = translation
+    this.changes = changes
+  }
+}
+
 c.queue([{
   uri: 'http://www.iciba.com/pig',
   jQuery: {
@@ -18,24 +26,33 @@ c.queue([{
       console.log(error)
     } else {
       const $ = res.$
-      const phoneticSymbol = $('.base-speak span').each(function() {
-        const val = $(this).find('span').text()
-        console.log(val)
+
+      const symbols = {}
+      $('.base-speak span span').each(function(idx) {
+        const val = $(this).text()
+        if (idx === 0) {
+          symbols['en'] = val
+        } else {
+          symbols['us'] = val
+        }
       })
 
-      const translation = $('.base-list.switch_part li').each(function() {
+      const translations = []
+      $('.base-list.switch_part li').each(function() {
         let content = ''
         $(this).find('span').each(function() {
           content += $(this).text()
         })
-        console.log(content)
+        translations.push(content)
       })
 
-      const arr = []
-      const change = $('.change.clearfix span').each(function() {
-        arr.push($(this).text() + $(this).find('a').text())
+      const changes = []
+      $('.change.clearfix span').each(function() {
+        changes.push($(this).text() + $(this).find('a').text())
       })
-      console.log(arr)
+
+      const word = new Word(symbols, translations, changes)
+      console.log(word)
     }
     done()
   }
