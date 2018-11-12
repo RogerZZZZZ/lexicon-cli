ac38ebe266ac440e25f8d486fe9bf2f2b42740b4
RogerZZZZZ <501841352@qq.com>
2018-11-12 18:55:11 +0800

    simplify the command

--- command.js
+++ command.js
@@ -9,27 +9,11 @@ const opt = {
 
 program
   .version('0.0.1')
-  .description('test description')
+  .option('-t, --theme', 'Choose another theme!')
+  .parse(process.argv)
 
-program
-  .command('query <word>')
-  .alias('q')
-  .description('Query <word>')
-  // .option('-f, --fuzzy', 'With fuzzy matching')
-  .action((word, cmd) => {
-    search(word, Object.assign(opt, cmd))
-  })
-
-program
-  .command('theme')
-  .alias('t')
-  .description('Choose another theme!')
-  .action(() => theme())
-
-program
-  .on('command:*', () => {
-    console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '))
-    process.exit(1)
-  })
-
-program.parse(process.argv)
+if (program.theme) {
+  theme()
+} else {
+  search(program.args.join(' '), opt)
+}
